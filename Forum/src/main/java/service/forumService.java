1569b9db7a9680268bb423e916c05b41b21127d3
hodaig <hodaig@gmail.com>
2015-04-18 22:17:50 +0300

    main interfaces # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # Explicit paths specified without -i nor -o; assuming --only paths... # On branch master # Your branch is ahead of 'origin/master' by 1 commit. #   (use "git push" to publish your local commits) # # Changes to be committed: #	modified:   Acceptance/pom.xml #

---
+++ Forum/src/main/java/service/forumService.java
@@ -0,0 +1,8 @@
+package service;
+
+/**
+ * interface to all the forum services (non-users)
+ * Created by hodai on 4/18/15.
+ */
+public interface forumService {
+}
---
+++ Forum/src/main/java/service/userService.java
@@ -0,0 +1,9 @@
+package service;
+
+/**
+ * interface for the user management functionality
+ *
+ * Created by hodai on 4/18/15.
+ */
+public interface userService {
+}
