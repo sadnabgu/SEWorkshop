d3eae8e59da86b2afd28042f48c0795e35f524e9
gurkashi <gurkashi@gmail.com>
2015-04-21 17:21:16 +0300

    added some acceptance tests

---
+++ Acceptance/src/test/java/org/bgu/stories/ForumCreation.java
@@ -0,0 +1,19 @@
+package org.bgu.stories;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by gur on 21/04/2015.
+ */
+public class ForumCreation {
+    @Test
+    @Ignore
+    public void createForum_SystemInitialized_NewForumInWaitingState(){
+        // TODO: Setup system to initial state
+
+        // TODO: Simulate Super-Admin clicks Create-Forum button
+
+        // TODO: Query system so it has a new forum in waiting state
+    }
+}
---
+++ Acceptance/src/test/java/org/bgu/stories/SetForumProperties.java
@@ -0,0 +1,21 @@
+package org.bgu.stories;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by gur on 21/04/2015.
+ */
+public class SetForumProperties {
+    @Test
+    @Ignore
+    public void setProperties_systemHasWaitingForum_PropertiesAreSetAndForumIsActive(){
+        //TODO: setup system to have a waiting forum and super admin
+
+        //TODO: simulate forum policy
+        //TODO: simulate forum moderators
+        //TODO: simulate activation
+
+        //TODO: query that the forum is active
+    }
+}
