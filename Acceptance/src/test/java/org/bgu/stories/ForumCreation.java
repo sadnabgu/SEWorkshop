90fcabc4ea356bc37647ad0c1a623cbbe5819554
sharonKerz <sharonkerz@gmail.com>
2015-04-24 02:23:31 +0300

    added Negative test for ForumCreation

--- Acceptance/src/test/java/org/bgu/stories/ForumCreation.java
+++ Acceptance/src/test/java/org/bgu/stories/ForumCreation.java
@@ -9,11 +9,43 @@ import org.junit.Test;
 public class ForumCreation {
     @Test
     @Ignore
-    public void createForum_SystemInitialized_NewForumInWaitingState(){
+    /*
+    *Test purpose: Forum is in waiting state after giving correct data
+    *
+    * Steps:
+    * 1. insert correct data
+    * 2. verify: forum is in Waiting state
+    *
+    *
+     */
+    public void createForumWithCorrectData_SystemInitialized_NewForumInWaitingState(){
         // TODO: Setup system to initial state
 
         // TODO: Simulate Super-Admin clicks Create-Forum button
 
-        // TODO: Query system so it has a new forum in waiting state
+        // TODO: Insert valid data
+
+        // TODO: verify : Query system so it has a new forum in waiting state
+    }
+
+    @Test
+    @Ignore
+    /*
+    *Test purpose: Forum is in not exist after giving incorrect data
+    *
+    * Steps:
+    * 1. insert incorrect data
+    * 2. verify: forum is in not exists
+    *
+    *
+     */
+    public void createForumWithIncorrectData_SystemInitialized_NewForumInNotExists() {
+        // TODO: Setup system to initial state
+
+        // TODO: Simulate Super-Admin clicks Create-Forum button
+
+        // TODO: Insert incorrect data
+
+        // TODO: Verify: forum is in not exists
     }
 }
