87a0cea2a459586c3535388126f35520e3fc68a3
sharonKerz <sharonkerz@gmail.com>
2015-04-24 02:34:15 +0300

    added tests for Login stories

---
+++ Acceptance/src/test/java/org/bgu/stories/usersStories/Login.java
@@ -0,0 +1,56 @@
+package org.bgu.stories.usersStories;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by gur on 21/04/2015.
+ */
+public class Login {
+    @Test
+    @Ignore
+    /*
+    *Test purpose: user is a member after login with correct data
+    *
+    * Steps:
+    * 1. insert correct data
+    * 2. verify: user is login with the same data name
+    *
+    *
+     */
+    public void UserLoginWithCorrectData_UserIsSignedUp_UserIsActive(){
+        // TODO: Setup system to initial state
+
+        // TODO: UserIsSignedInAndLogout
+
+        // TODO: Simulate guest clicks login button
+
+        // TODO: Insert valid data
+
+        // TODO: verify : Query system so user is now log in
+    }
+
+    @Test
+    @Ignore
+    /*
+    *Test purpose: user is a guest after login with incorrect data
+    *
+    * Steps:
+    * 1. insert incorrect data
+    * 2. verify: user is a guest
+    *
+    *
+     */
+    public void UserLoginWithIncorrectData_UserIsSignedUp_UserIsAGuest(){
+        // TODO: Setup system to initial state
+
+        // TODO: User Is Signed In And Logout
+
+        // TODO: Simulate guest clicks login button
+
+        // TODO: Insert invalid data
+
+        // TODO: verify : Query system so user is guest
+    }
+
+}
