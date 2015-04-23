4ff5ccf456ac5c8c2e97eff7c8edfbed694b0b6f
sharonKerz <sharonkerz@gmail.com>
2015-04-24 02:39:09 +0300

    added tests for SignUp stories

---
+++ Acceptance/src/test/java/org/bgu/stories/usersStories/SignUp.java
@@ -0,0 +1,51 @@
+package org.bgu.stories.usersStories;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by Sharon Kerzman on 24/04/2015.
+ */
+public class SignUp {
+    @Test
+    @Ignore
+    /*
+    *Test purpose: user is a member after signing up with correct data
+    *
+    * Steps:
+    * 1. insert correct data
+    * 2. verify: user is Signed up and login
+    *
+    *
+     */
+    public void UserSignUpWithCorrectData_UserIsSignedUp_UserIsActive(){
+        // TODO: Setup system to initial state
+
+        // TODO: Simulate guest clicks sign up button
+
+        // TODO: Insert valid data
+
+        // TODO: verify : Query system so user is now log in and exists
+    }
+
+    @Test
+    @Ignore
+    /*
+    *Test purpose: user is a guest after signed up with incorrect data
+    *
+    * Steps:
+    * 1. insert incorrect data
+    * 2. verify: user is a guest
+    *
+    *
+     */
+    public void UserLoginWithIncorrectData_UserIsSignedUp_UserIsAGuest(){
+        // TODO: Setup system to initial state
+
+        // TODO: Simulate guest clicks sign up button
+
+        // TODO: Insert invalid data
+
+        // TODO: verify : Query system so user is guest and not exists
+    }
+}
