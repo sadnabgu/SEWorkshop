58d05adc327ba40aa7cd4b7f31ea639d81faf5fe
sharonKerz <sharonkerz@gmail.com>
2015-04-24 02:47:19 +0300

    added tests for sub forum creation stories

---
+++ Acceptance/src/test/java/org/bgu/stories/SubForumCreation.java
@@ -0,0 +1,54 @@
+package org.bgu.stories;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by Sharon Kerzman on 24/04/2015.
+ */
+public class SubForumCreation {
+    @Test
+    @Ignore
+    /*
+    *Test purpose: Sub Forum is in created after giving correct data
+    *
+    * Steps:
+    * 1. insert correct data
+    * 2. verify: sub forum is created
+    *
+    *
+     */
+    public void createSubForumWithCorrectData_ForumCreation_NewSubForumIsCreated(){
+        // TODO: Setup system to initial state
+
+        // TODO: Forum creation
+
+        // TODO: Simulate Super-Admin clicks Create Sub Forum button
+
+        // TODO: Insert valid data
+
+        // TODO: verify : Query system so it has a new Sub forum is created
+    }
+
+        @Test
+        @Ignore
+    /*
+    *Test purpose: Sub Forum is not exists after giving incorrect data
+    *
+    * Steps:
+    * 1. insert correct data
+    * 2. verify: sub forum is not exists
+    *
+     */
+        public void createSubForumWithInCorrectData_ForumCreation_NewSubForumIsNotCreated(){
+            // TODO: Setup system to initial state
+
+            // TODO: Forum creation
+
+            // TODO: Simulate Super-Admin clicks Create Sub Forum button
+
+            // TODO: Insert invalid data
+
+            // TODO: verify : new Sub Forum is not exists
+        }
+}
