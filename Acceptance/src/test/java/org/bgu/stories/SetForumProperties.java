4a3c17659e997af6db42abe4b1d74b28a5184c68
sharonKerz <sharonkerz@gmail.com>
2015-04-24 02:18:21 +0300

    added Negative test for setForumProperties

--- Acceptance/src/test/java/org/bgu/stories/SetForumProperties.java
+++ Acceptance/src/test/java/org/bgu/stories/SetForumProperties.java
@@ -9,13 +9,48 @@ import org.junit.Test;
 public class SetForumProperties {
     @Test
     @Ignore
-    public void setProperties_systemHasWaitingForum_PropertiesAreSetAndForumIsActive(){
+    /*
+    *Test purpose: Forum is active after giving correct data
+    *
+    * Steps:
+    * 1. insert correct data
+    * 2. verify: forum is active
+    *
+    *
+     */
+    public void setPropertiesWithCorrectData_systemHasWaitingForum_PropertiesAreSetAndForumIsActive(){
         //TODO: setup system to have a waiting forum and super admin
 
         //TODO: simulate forum policy
         //TODO: simulate forum moderators
+
+        //TODO: Policy data are correct
         //TODO: simulate activation
 
-        //TODO: query that the forum is active
+        //TODO: verify: query that the forum is active
     }
+
+    @Test
+    @Ignore
+    /*
+    *Test purpose: Forum is waiting after giving incorrect data
+    *
+    * Steps:
+    * 1. insert incorrect data
+    * 2. verify: forum is in Waiting mode
+    *
+    *
+     */
+    public void setPropertiesWithIncorrectData_systemHasWaitingForum_PropertiesAreNotSetAndForumIsWaiting(){
+        //TODO: setup system to have a waiting forum and super admin
+
+        //TODO: simulate forum policy
+        //TODO: simulate forum moderators
+
+        //TODO: Policy data are incorrect
+        //TODO: simulate activation
+
+        //TODO: verify: query that the forum is in waiting mode
+    }
+
 }
