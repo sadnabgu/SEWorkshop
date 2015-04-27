21f28fc722904a66c7de346f18714126948f8cf0
sharonKerz <sharonkerz@gmail.com>
2015-04-27 21:55:38 +0300

    added tests for Delete forum

---
+++ Acceptance/src/main/test/java/org/bgu/stories/DeleteForum.java
@@ -0,0 +1,29 @@
+package org.bgu.stories;
+
+import org.bgu.domain.facades.ForumFacade;
+import org.bgu.domain.model.Forum;
+import org.junit.Ignore;
+import org.junit.Test;
+import java.lang.System;
+
+/**
+ * Created by gur on 21/04/2015.
+ *
+ */
+public class ForumCreation {
+    @Test
+    @Ignore
+    /*
+    *Test purpose: Delete Forum succesfuly by admin
+    *
+    * Steps:
+    * 1. delete forum
+    * 2. verify: forum is in Waiting state
+    *
+    *
+     */
+    public void DeleteForum_SystemInitialized_ForumAsBeenDeleted(){
+
+    }
+
+}
