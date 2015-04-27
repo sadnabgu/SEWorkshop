58079fb2cdd9264a4029def10e0df662e0606914
sharonKerz <sharonkerz@gmail.com>
2015-04-27 21:52:49 +0300

    added tests for Delete message

---
+++ Acceptance/src/main/test/java/org/bgu/stories/usersStories/DeleteMessage.java
@@ -0,0 +1,61 @@
+package org.bgu.stories.usersStories;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by Sharon Kerzman on 24/04/2015.
+ */
+public class DeleteMessage {
+    @Test
+    @Ignore
+    /*
+    *Test purpose: Delete message by owner member user
+    *
+    * Steps:
+    * 1. delete message
+    * 2. verify: message is deleted
+    *
+    *
+     */
+    public void deleteMessageByOwnerMember_UserIsSignedUp_MessageIsDeleted(){
+        // TODO: Setup system to initial state
+
+        // TODO: Forum snd subFOrum is creTED
+
+        // TODO: UserIsSignedIn
+
+        // TODO: write message
+
+        // TODO: Simulate  clicks delete message button
+
+        // TODO: verify : Query system so Message is deleted
+    }
+
+    @Test
+    @Ignore
+    /*
+    *Test purpose: can not Delete message by not owner member
+    *
+    * Steps:
+    * 1. delete message by not owner member
+    * 2. verify: message is not deleted
+    *
+    *
+     */
+    public void CanNotDeleteMessageByNotOwnerMember_UserIsSignedUp_MessageIsNotDeleted(){
+        // TODO: Setup system to initial state
+
+        // TODO: Forum snd subFOrum is creTED
+
+        // TODO: UserIsSignedIn
+
+        // TODO: write message
+
+        // TODO: loging with another member
+
+        // TODO: Simulate  clicks delete message button
+
+        // TODO: verify : Query system so Message is not deleted
+    }
+}
