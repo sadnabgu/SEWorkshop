58626d40ea39eeb1c8f2c8ebb5264ec722587193
sharonKerz <sharonkerz@gmail.com>
2015-04-27 20:49:23 +0300

    added tests for member types

---
+++ Acceptance/src/main/test/java/org/bgu/stories/MemberTypes.java
@@ -0,0 +1,51 @@
+package org.bgu.stories.usersStories;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Created by Sharon Kerzman on 24/04/2015.
+ */
+public class MemberType {
+    @Test
+    @Ignore
+    /*
+    *Test purpose: admin setting up member types with correct data - verify types are declared
+    *
+    * Steps:
+    * 1. insert correct data
+    * 2. verify: member types declared
+    *
+    *
+     */
+    public void MemberTypeDeclarationWithCorrectData_ForumInit_MemberTypesDeclared(){
+        // TODO: Setup system to initial state
+
+        // TODO: Simulate admin clicks member type button
+
+        // TODO: Insert valid data
+
+        // TODO: verify : Query system so member types have been created
+    }
+
+    @Test
+    @Ignore
+    /*
+    *Test purpose: admin setting up member types with incorrect data - verify types are declared
+    *
+    * Steps:
+    * 1. insert correct data
+    * 2. verify: member types declared
+    *
+    *
+     */
+    public void MemberTypeDeclarationWithIncorrectData_ForumInit_MemberTypesDeclared(){
+        // TODO: Setup system to initial state
+
+        // TODO: Simulate admin clicks member type button
+
+        // TODO: Insert invalid data
+
+        // TODO: verify : Query system so member types not been created
+    }
+}
