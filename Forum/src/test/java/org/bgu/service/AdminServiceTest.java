0059b76d40ceb03e1958fd14f5a1964a0c204a9a
sharonKerz <sharonkerz@gmail.com>
2015-06-17 23:11:22 +0300

    fixed bugs

--- Forum/src/test/java/org/bgu/service/AdminServiceTest.java
+++ Forum/src/test/java/org/bgu/service/AdminServiceTest.java
@@ -28,7 +28,7 @@ public class AdminServiceTest {
     public static void initialSystem() {
         adminService = new AdminService();
         adminService.resetSystem();
-        assertEquals(Result.SUCCESS, adminService.initializeSystem(ADMIN1_NAME, ADMIN1_PASS));
+        assertEquals(Result.SUCCESS, adminService.initializeSystem(ADMIN1_NAME, ADMIN1_PASS)._result);
     }
 
     @Before
@@ -39,23 +39,23 @@ public class AdminServiceTest {
 
     @Test
     public void loginSystem() {
-        assertEquals(Result.SUCCESS, adminService.loginSystem(ADMIN1_NAME, ADMIN1_PASS));
+        assertEquals(Result.SUCCESS, adminService.loginSystem(ADMIN1_NAME, ADMIN1_PASS)._result);
     }
 
     @Test
     public void initializeSystem_initialSequence_pass() {
         adminService.resetSystem();
-        assertEquals(Result.SUCCESS, adminService.initializeSystem(ADMIN1_NAME, ADMIN1_PASS));
+        assertEquals(Result.SUCCESS, adminService.initializeSystem(ADMIN1_NAME, ADMIN1_PASS)._result);
         logoutSystem();
-        assertEquals(Result.SUCCESS, adminService.loginSystem(ADMIN1_NAME, ADMIN1_PASS));
+        assertEquals(Result.SUCCESS, adminService.loginSystem(ADMIN1_NAME, ADMIN1_PASS)._result);
     }
 
     @Test
     public void initializeSystem_initialSequence_fail() {
         adminService.resetSystem();
 
-        assertEquals(Result.UNINITIALIZED_SYSTEM, adminService.loginSystem(ADMIN1_NAME, ADMIN1_PASS));
-        assertEquals(Result.SUCCESS, adminService.initializeSystem(ADMIN1_NAME, ADMIN1_PASS));
+        assertEquals(Result.UNINITIALIZED_SYSTEM, adminService.loginSystem(ADMIN1_NAME, ADMIN1_PASS)._result);
+        assertEquals(Result.SUCCESS, adminService.initializeSystem(ADMIN1_NAME, ADMIN1_PASS)._result);
 
     }
 
@@ -63,17 +63,17 @@ public class AdminServiceTest {
     public void logginSystem_initialSequence_fail() {
 
         logoutSystem();
-        assertEquals(Result.FAIL, adminService.loginSystem(ADMIN1_NAME, "wrongPass"));
-        assertEquals(Result.FAIL, adminService.loginSystem("wrongName", ADMIN1_PASS));
-        assertEquals(Result.FAIL, adminService.loginSystem("wrongName", "wrongPass"));
+        assertEquals(Result.FAIL, adminService.loginSystem(ADMIN1_NAME, "wrongPass")._result);
+        assertEquals(Result.FAIL, adminService.loginSystem("wrongName", ADMIN1_PASS)._result);
+        assertEquals(Result.FAIL, adminService.loginSystem("wrongName", "wrongPass")._result);
     }
 
     @Test
     public void createForum_createFunction_pass() {
         loginSystem();
 
-        assertEquals(Result.SUCCESS, adminService.createForum("form1", MANAGER1_NAME, MANAGER1_PASS));
-        assertEquals(Result.SUCCESS, adminService.createForum("form2", MANAGER2_NAME, MANAGER2_PASS));
+        assertEquals(Result.SUCCESS, adminService.createForum("form1", MANAGER1_NAME, MANAGER1_PASS)._result);
+        assertEquals(Result.SUCCESS, adminService.createForum("form2", MANAGER2_NAME, MANAGER2_PASS)._result);
         //TODO - test forum creation throw the ForumService
     }
 
@@ -81,15 +81,15 @@ public class AdminServiceTest {
     public void createForum_createFunction_fail() {
         // create forum while not logedin
         logoutSystem();
-        assertEquals(Result.NOT_LOGGEDIN_SYSTEM, adminService.createForum("form1", MANAGER1_NAME, MANAGER1_PASS));
+        assertEquals(Result.NOT_LOGGEDIN_SYSTEM, adminService.createForum("form1", MANAGER1_NAME, MANAGER1_PASS)._result);
         loginSystem();
 
 
-            assertEquals(Result.SUCCESS, adminService.createForum("form1", MANAGER1_NAME, MANAGER1_PASS));
-            assertEquals(Result.FORUM_EXISTS, adminService.createForum("form1", MANAGER2_NAME, MANAGER2_PASS));
+            assertEquals(Result.SUCCESS, adminService.createForum("form1", MANAGER1_NAME, MANAGER1_PASS)._result);
+            assertEquals(Result.FORUM_EXISTS, adminService.createForum("form1", MANAGER2_NAME, MANAGER2_PASS)._result);
         // the error not affect the system (still can create forums)
 
-            assertEquals(Result.SUCCESS, adminService.createForum("newForumName", MANAGER1_NAME, MANAGER1_PASS));
+            assertEquals(Result.SUCCESS, adminService.createForum("newForumName", MANAGER1_NAME, MANAGER1_PASS)._result);
 
 
         //TODO - test that forum wasn't changed through the ForumService
