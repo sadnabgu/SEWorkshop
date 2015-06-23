6dedc74431ec77767d19bc82a3ed596f1a5340b4
sharonKerz <sharonkerz@gmail.com>
2015-06-24 01:20:49 +0300

    delete forum tests

--- Acceptance/src/test/java/org/bgu/stories/ForumStories/DeleteForum.java
+++ Acceptance/src/test/java/org/bgu/stories/ForumStories/DeleteForum.java
@@ -1,18 +1,17 @@
 package org.bgu.stories.ForumStories;
 
-import org.bgu.domain.facades.ForumFacade;
-import org.bgu.domain.model.Forum;
-import org.junit.Ignore;
+import org.bgu.ForumCreatedTestBase;
 import org.junit.Test;
-import java.lang.System;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by gur on 21/04/2015.
  *
  */
-public class DeleteForum {
-    @Test
-    @Ignore
+public class DeleteForum extends ForumCreatedTestBase {
+
     /*
     *Test purpose: Delete Forum succesfuly by admin
     *
@@ -22,8 +21,30 @@ public class DeleteForum {
     *
     *
      */
-    public void DeleteForum_SystemInitialized_ForumAsBeenDeleted(){
-
+    @Test
+    public void DeleteForum_byAdmin_pass_testID_10_1(){
+        assertTrue("could not log out admin", bridge.login(ADMIN1_NAME,ADMIN1_PASS));
+        assertTrue("could not delete forum", bridge.deleteForum(FORUM_NAME));
+    }
+    @Test
+    public void DeleteForum_byManager_fail_testID_10_2(){
+        assertTrue("could not log out admin", bridge.login(MANAGER_NAME,MANAGER_PASS));
+        assertFalse("manager can delete forum", bridge.deleteForum(FORUM_NAME));
+    }
+    @Test
+    public void DeleteForum_byMemebr_fail_testID_10_3(){
+        assertTrue("could not log out admin", bridge.login(MEMBER_NAME,MEMBER_PASS));
+        assertFalse("member can delete forum", bridge.deleteForum(FORUM_NAME));
+    }
+    @Test
+    public void DeleteForum_byGuest_fail_testID_10_4(){
+        assertFalse("guest can delete forum", bridge.deleteForum(FORUM_NAME));
+    }
+    @Test
+    public void DeleteForum_byAdmin_DeleteTwice_pass_testID_10_5(){
+        assertTrue("can not log out admin", bridge.login(ADMIN1_NAME,ADMIN1_PASS));
+        assertTrue("can not delete forum", bridge.deleteForum(FORUM_NAME));
+        assertFalse("can delete forum twice", bridge.deleteForum(FORUM_NAME));
     }
 
 }
