ec26d531eb42ca9731813fa4fe47a656dbe26e3e
sharonKerz <sharonkerz@gmail.com>
2015-06-24 01:21:12 +0300

    init forum tests

---
+++ Acceptance/src/test/java/org/bgu/stories/ForumStories/InitializeForum.java
@@ -0,0 +1,28 @@
+package org.bgu.stories.ForumStories;
+
+import org.bgu.TestBase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Sharon Kerzman on 23/06/2015.
+ */
+public class InitializeForum extends TestBase {
+    @Test
+    public void initializeSystem_pass_testID_1_1() {
+        assertTrue("system is not initialized", bridge.initSystem(ADMIN1_NAME, ADMIN1_PASS));
+    }
+
+    @Test
+    public void initializeSystem_noUserName_false_testID_1_2() {
+        assertFalse("system is initialized when is should not be", bridge.initSystem("", ADMIN1_PASS));
+    }
+
+    @Test
+    public void initializeSystem_noPassword_false_testID_1_1() {
+        assertFalse("system is initialized when is should not be", bridge.initSystem(ADMIN1_NAME, ""));
+    }
+
+}
