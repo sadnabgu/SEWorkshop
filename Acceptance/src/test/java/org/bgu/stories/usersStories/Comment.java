8710fe09fe28479a05b11482255f43a12d2c713f
sharonKerz <sharonkerz@gmail.com>
2015-06-24 19:21:25 +0300

    create new thread and comments changes

--- Acceptance/src/test/java/org/bgu/stories/usersStories/Comment.java
+++ Acceptance/src/test/java/org/bgu/stories/usersStories/Comment.java
@@ -1,87 +1,82 @@
 package org.bgu.stories.usersStories;
 
-import org.junit.Ignore;
+import org.bgu.NewThreadCreatedTestBase;
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Sharon Kerzman on 24/04/2015.
  */
-public class Comment {
+public class Comment extends NewThreadCreatedTestBase {
+    int memberCommentId;
+
     @Test
-    @Ignore
-    /*
-    *Test purpose: second message is published after sending correct data by member user
-    *
-    * Steps:
-    * 1. insert correct data
-    * 2. verify: message is published
-    *
-    *
-     */
-    public void WriteSecondMessageWithValidDataByMember_FirstMessageCreated_SecondMessageIsPublished(){
-        // TODO: Setup system to initial state
-
-        // TODO: Forum snd subFOrum is creTED
-
-        // TODO: UserIsSignedIn
-
-        //TODO: CreateNewThread
-
-        // TODO: Simulate guest clicks write message button
-
-        // TODO: Insert valid data
-
-        // TODO: verify : Query system so Message is published
+    public void WriteCommentMessage_TitleAndMsgExsits_pass_testID_6_1() {
+        assertTrue("can not Login member", bridge.login(MEMBER_NAME, MEMBER_PASS));
+        memberCommentId = bridge.createNewComment(newThreadId, COMMENT_TITLE, COMMENT_BODY);
+        assertTrue("member can not add new comment", memberCommentId > 0);
     }
 
     @Test
-    @Ignore
-    /*
-    *Test purpose: Second message is not published after sending incorrect data by member user
-    *
-    * Steps:
-    * 1. insert incorrect data
-    * 2. verify: message is not published
-    *
-    *
-     */
-    public void WriteSecondMessageWithiInvalidDataByMember_FirstMessageCreated_MessageIsNotPublished(){
-        // TODO: Setup system to initial state
-
-        // TODO: Forum snd subFOrum is creTED
-
-        // TODO: UserIsSignedIn
-
-        // TODO: First message created
-
-        // TODO: Simulate Member clicks write message button
-
-        // TODO: Insert invalid data
-
-        // TODO: verify : Query system so Message is not published
+    public void WriteCommentMessage_TitleMissing_pass_testID_6_2() {
+        assertTrue("can not Login member", bridge.login(MEMBER_NAME, MEMBER_PASS));
+        memberCommentId = bridge.createNewComment(newThreadId, "", COMMENT_BODY);
+        assertTrue("member can not add new comment without title", memberCommentId > 0);
     }
 
+    @Test
+    public void WriteCommentMessage_MsgMissing_pass_testID_6_3() {
+        assertTrue("can not Login member", bridge.login(MEMBER_NAME, MEMBER_PASS));
+        memberCommentId = bridge.createNewComment(newThreadId, COMMENT_TITLE, "");
+        assertTrue("member can not add new comment without body", memberCommentId > 0);
+    }
 
     @Test
-    @Ignore
+    public void WriteCommentMessage_bothMissing_fail_testID_6_4() {
+        assertTrue("can not Login member", bridge.login(MEMBER_NAME, MEMBER_PASS));
+        memberCommentId = bridge.createNewComment(newThreadId, "", "");
+        assertFalse("member can add new comment with no title and body", memberCommentId > 0);
+    }
+
+    @Test
+    public void WriteCommentMessage_byGuest_fail_testID_6_5() {
+        memberCommentId = bridge.createNewComment(newThreadId, COMMENT_TITLE, COMMENT_BODY);
+        assertFalse("guest can add comments threads", memberCommentId <= 0);
+    }
+
     /*
-    *Test purpose: Second message is not published by guest user
-    *
-    * Steps:
-    * 1. verify: message is not published
-    *
-    *
+    1. owner member comment
+    2. another member (member 2) comment
+    3. admin comment to owner member
+    4. manager comment to member 2
      */
-    public void ClickFirstMessageByGuest_SubForumCreated_MessageIsNotPublished(){
-        // TODO: Setup system to initial state
+    @Test
+    public void WriteCommentMessage_ManyComments_pass_testID_6_1() {
+        int otherMemberComment;
+        int adminComment;
+        int managerComment;
 
-        // TODO: Forum snd subFOrum is creTED
+        assertTrue("can not Login member", bridge.login(MEMBER_NAME, MEMBER_PASS));
+        memberCommentId = bridge.createNewComment(newThreadId, COMMENT_TITLE, COMMENT_BODY);
+        assertTrue("member can not add new comment", memberCommentId > 0);
+        assertTrue("can not Logout member", bridge.logout());
 
-        // TODO: First message created
+        assertTrue("can not Login member", bridge.login(MEMBER_2_NAME, MEMBER_2_PASS));
+        otherMemberComment = bridge.createNewComment(newThreadId, COMMENT_TITLE, COMMENT_BODY);
+        assertTrue("member can not add new comment", otherMemberComment > 0);
+        assertTrue("can not Logout member", bridge.logout());
 
-        // TODO: Simulate guest clicks write message button
+        assertTrue("can not Login member", bridge.login(ADMIN1_NAME, ADMIN1_PASS));
+        adminComment = bridge.createNewComment(memberCommentId, COMMENT_TITLE, COMMENT_BODY);
+        assertTrue("member can not add new comment", adminComment > 0);
+        assertTrue("can not Logout member", bridge.logout());
 
-        // TODO: verify : Query system so Message is not published
+        assertTrue("can not Login member", bridge.login(MEMBER_NAME, MEMBER_PASS));
+        managerComment = bridge.createNewComment(otherMemberComment, COMMENT_TITLE, COMMENT_BODY);
+        assertTrue("member can not add new comment", managerComment > 0);
+        assertTrue("can not Logout member", bridge.logout());
     }
 
 }
---
+++ Acceptance/src/test/java/org/bgu/stories/usersStories/CreateNewThread.java
@@ -0,0 +1,49 @@
+package org.bgu.stories.usersStories;
+
+import org.bgu.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Sharon Kerzman on 24/04/2015.
+ */
+public class CreateNewThread extends SubForumCreatedTestBase {
+    int firstMessageId;
+
+    @Test
+    public void WriteFirstMessage_TitleAndMsgExsits_pass_testID_6_1() {
+        assertTrue("can not Login manager", bridge.login(MEMBER_NAME, MEMBER_PASS));
+        firstMessageId = bridge.createNewThread(THREAD_TITLE, THREAD_BODY);
+        assertTrue("member can not add new thread", firstMessageId > 0);
+    }
+
+    @Test
+    public void WriteFirstMessage_TitleMissing_pass_testID_6_2() {
+        assertTrue("can not Login manager", bridge.login(MEMBER_NAME, MEMBER_PASS));
+        firstMessageId = bridge.createNewThread("", THREAD_BODY);
+        assertTrue("member can not add new thread without title", firstMessageId > 0);
+    }
+
+    @Test
+    public void WriteFirstMessage_MsgMissing_pass_testID_6_3() {
+        assertTrue("can not Login manager", bridge.login(MEMBER_NAME, MEMBER_PASS));
+        firstMessageId = bridge.createNewThread(THREAD_TITLE, "");
+        assertTrue("member can not add new thread without body", firstMessageId > 0);
+    }
+
+    @Test
+    public void WriteFirstMessage_bothMissing_fail_testID_6_4() {
+        assertTrue("can not Login manager", bridge.login(MEMBER_NAME, MEMBER_PASS));
+        firstMessageId = bridge.createNewThread("", "");
+        assertFalse("member can add new thread without title and body", firstMessageId <= 0);
+    }
+
+    @Test
+    public void WriteFirstMessage_byGuest_fail_testID_6_5() {
+        firstMessageId = bridge.createNewThread(THREAD_TITLE, THREAD_BODY);
+        assertFalse("guest can add new threads", firstMessageId <= 0);
+    }
+
+}
