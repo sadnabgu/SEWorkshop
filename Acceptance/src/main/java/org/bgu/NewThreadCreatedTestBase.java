6c2d3272c1e2265f8a414725c766e319605ebcbc
sharonKerz <sharonkerz@gmail.com>
2015-06-24 19:22:23 +0300

    refactored message to thread

---
+++ Acceptance/src/main/java/org/bgu/NewThreadCreatedTestBase.java
@@ -0,0 +1,25 @@
+package org.bgu;
+
+import org.junit.Before;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Sharon Kerzman on 24/06/2015.
+ */
+public class NewThreadCreatedTestBase extends SubForumCreatedTestBase {
+
+    public static final String COMMENT_TITLE = "OUT OF DATE MAN! OUD!!!";
+    public static final String COMMENT_BODY = "do not touch it and give it to the next homeless ASAP!";
+    protected int newThreadId;
+
+    @Before
+    public void init() {
+        super.init();
+        assertTrue("can not Login member", bridge.login(MEMBER_NAME, MEMBER_PASS));
+        newThreadId = bridge.createNewThread(THREAD_TITLE, THREAD_BODY);
+        assertTrue("can not create new thread", newThreadId > 0);
+        assertTrue("can not logout member", bridge.logout());
+        assertTrue("can not register second member", bridge.register(MEMBER_2_NAME, MEMBER_2_PASS));
+    }
+}
