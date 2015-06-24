916bd8c0a769e3d435e2662c32d6eaad343ed11b
sharonKerz <sharonkerz@gmail.com>
2015-06-24 20:54:21 +0300

    add moderate

---
+++ Acceptance/src/test/java/org/bgu/stories/ForumStories/AddModerate.java
@@ -0,0 +1,42 @@
+package org.bgu.stories.ForumStories;
+
+import org.bgu.SubForumCreatedTestBase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Sharon Kerzman on 24/06/2015.
+ */
+public class AddModerate extends SubForumCreatedTestBase {
+
+    @Test
+    public void addModerate_byManager_pass_testID_6_1() {
+        assertTrue("can not Login manager", bridge.login(MANAGER_NAME, MANAGER_PASS));
+        assertTrue("can not add moderate", bridge.addModerate(FORUM_NAME, SUBFORUM_NAME, MODERATE_2_NAME));
+    }
+
+    @Test
+    public void addModerate_byAdmin_pass_testID_6_1() {
+        assertTrue("can not Login admin", bridge.login(ADMIN1_NAME, ADMIN1_PASS));
+        assertTrue("can not add moderate", bridge.addModerate(FORUM_NAME, SUBFORUM_NAME, MODERATE_2_NAME));
+    }
+
+    @Test
+    public void addModerate_alreadyModerate_fail_testID_6_1() {
+        assertTrue("can not Login admin", bridge.login(MANAGER_NAME, MANAGER_PASS));
+        assertFalse("can add existing moderate", bridge.addModerate(FORUM_NAME, SUBFORUM_NAME, MODERATE_NAME));
+    }
+
+    @Test
+    public void addModerate_byMember_fail_testID_6_1() {
+        assertTrue("can not Login admin", bridge.login(MEMBER_NAME, MEMBER_PASS));
+        assertFalse("member can add moderate", bridge.addModerate(FORUM_NAME, SUBFORUM_NAME, MODERATE_2_NAME));
+    }
+
+    @Test
+    public void addModerate_byGuest_fail_testID_6_1() {
+        assertFalse("guest can add moderate", bridge.addModerate(FORUM_NAME, SUBFORUM_NAME, MODERATE_2_NAME));
+    }
+}
